0525090abbca82458fe9958fcab85732bb40ea97
ayavryk <ayavryk@gmail.com>
2017-03-29 14:48:58 +0300

    home page

--- app/containers/HomePage/home.tsx
+++ app/containers/HomePage/home.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
+import { config as appConfig } from 'lib/appConfig';
+import { hashHistory } from 'react-router';
 export default class HomePage extends React.Component<any, void> {
+    public componentDidMount(){
+        hashHistory.push(appConfig.homePage);
+    }
     public render() {
         return (
             <div/>
